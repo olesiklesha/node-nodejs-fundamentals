1e95e8dd50c301af83e0b45f7eaf232d0ad66e98
Aliaksei Olesik <AOlesik@alfabank.ru>
2026-03-09 00:33:32 +0300

    feat(*): solve fitler task

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,22 @@
+import process from 'node:process'
+import {Transform} from 'node:stream';
+
+const [pattern] = process.argv.slice(3)
+
+
+class Filterer extends Transform {
+    _transform(chunk, encoding, callback) {
+        if (chunk.toString().includes(pattern)) {
+            this.push(chunk.toString());
+        }
+
+        callback();
+    }
+}
+
+
 const filter = () => {
-  // Write your code here
-  // Read from process.stdin
-  // Filter lines by --pattern CLI argument
-  // Use Transform Stream
-  // Write to process.stdout
+    process.stdin.pipe(new Filterer()).pipe(process.stdout);
 };
 
 filter();
